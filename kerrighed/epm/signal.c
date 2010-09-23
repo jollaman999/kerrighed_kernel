5763ce3b5b1eaa6a66639679bd8398b39218edc1
Matthieu Fertré <matthieu.fertre@kerlabs.com>
2010-09-23 11:49:17 +0200

    epm: Use epm_error() or app_error() instead of ckpt_err()

--- kerrighed/epm/signal.c
+++ kerrighed/epm/signal.c
@@ -829,6 +829,10 @@ int export_signal_struct(struct epm_action *action,
 	}
 
 err_write:
+	if (r)
+		epm_error(action, r, tsk,
+			  "Fail to save struct signal_struct");
+
 	return r;
 }
 
@@ -1030,6 +1034,10 @@ err_free_signal:
 	}
 
 err_read:
+	if (r)
+		epm_error(action, r, tsk,
+			  "Fail to restore struct signal_struct");
+
 	return r;
 }
 
@@ -1048,13 +1056,7 @@ static int cr_export_now_signal_struct(struct epm_action *action,
 				       struct task_struct *task,
 				       union export_args *args)
 {
-	int r;
-	r = export_signal_struct(action, ghost, task);
-	if (r)
-		ckpt_err(action, r,
-			 "Fail to save struct signal_struct of process %d (%s)",
-			 task_pid_knr(task), task->comm);
-	return r;
+	return export_signal_struct(action, ghost, task);
 }
 
 static int cr_import_now_signal_struct(struct epm_action *action,
@@ -1068,13 +1070,8 @@ static int cr_import_now_signal_struct(struct epm_action *action,
 	BUG_ON(*returned_data != NULL);
 
 	r = import_signal_struct(action, ghost, fake);
-	if (r) {
-		ckpt_err(action, r,
-			 "App %ld - Fail to restore a struct signal_struct",
-			 action->restart.app->app_id);
+	if (r)
 		goto err;
-	}
-
 	*returned_data = fake->signal;
 err:
 	return r;
--- kerrighed/fs/mobility.c
+++ kerrighed/fs/mobility.c
@@ -716,6 +716,10 @@ exit_put_files:
 	put_files_struct (exported_files);
 
 err:
+	if (r)
+		epm_error(action, r, tsk,
+			  "Fail to save struct files_struct");
+
 	return r;
 }
 
@@ -827,6 +831,10 @@ int export_fs_struct (struct epm_action *action,
 
 err_write:
 	free_page ((unsigned long) tmp);
+
+	if (r)
+		epm_error(action, r, tsk,
+			  "Fail to save struct fs_struct");
 
 	return r;
 }
@@ -1166,6 +1174,9 @@ exit_free_fdt:
 
 exit_free_files:
 	kmem_cache_free(files_cachep, files);
+	epm_error(action, r, tsk,
+		  "Fail to restore struct files_struct");
+
 	return r;
 }
 
@@ -1448,7 +1459,9 @@ int import_fs_struct (struct epm_action *action,
 
 exit:
 	free_page ((unsigned long) buffer);
-
+	if (r)
+		epm_error(action, r, tsk,
+			  "Fail to restore struct fs_struct");
 	return r;
 
 exit_put_root:
@@ -1515,14 +1528,7 @@ static int cr_export_now_files_struct(struct epm_action *action, ghost_t *ghost,
 				      struct task_struct *task,
 				      union export_args *args)
 {
-	int r;
-	r = export_files_struct(action, ghost, task);
-	if (r)
-		ckpt_err(action, r,
-			 "Fail to save struct files_struct of process %d (%s)",
-			 task_pid_knr(task), task->comm);
-
-	return r;
+	return export_files_struct(action, ghost, task);
 }
 
 static int cr_import_now_files_struct(struct epm_action *action, ghost_t *ghost,
@@ -1533,12 +1539,8 @@ static int cr_import_now_files_struct(struct epm_action *action, ghost_t *ghost,
 	BUG_ON(*returned_data != NULL);
 
 	r = import_files_struct(action, ghost, fake);
-	if (r) {
-		ckpt_err(action, r,
-			 "Fail to restore a struct files_struct",
-			 action->restart.app->app_id);
+	if (r)
 		goto err;
-	}
 
 	*returned_data = fake->files;
 err:
@@ -1578,14 +1580,7 @@ static int cr_export_now_fs_struct(struct epm_action *action, ghost_t *ghost,
 				   struct task_struct *task,
 				   union export_args *args)
 {
-	int r;
-	r = export_fs_struct(action, ghost, task);
-	if (r)
-		ckpt_err(action, r,
-			 "Fail to save struct fs_struct of process %d (%s)",
-			 task_pid_knr(task), task->comm);
-
-	return r;
+	return export_fs_struct(action, ghost, task);
 }
 
 static int cr_import_now_fs_struct(struct epm_action *action, ghost_t *ghost,
@@ -1596,13 +1591,8 @@ static int cr_import_now_fs_struct(struct epm_action *action, ghost_t *ghost,
 	BUG_ON(*returned_data != NULL);
 
 	r = import_fs_struct(action, ghost, fake);
-	if (r) {
-		ckpt_err(action, r,
-			 "App %ld - Fail to restore a struct fs_struct",
-			 action->restart.app->app_id);
+	if (r)
 		goto err;
-	}
-
 	*returned_data = fake->fs;
 err:
 	return r;
