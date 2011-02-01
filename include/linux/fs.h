afd55c9103ba88e58b4e09547d539815f740bfc6
Namhyung Kim <namhyung@gmail.com>
2011-02-01 15:52:46 -0800

    vfs: sparse: remove a warning on OPEN_FMODE()
    AND-ing FMODE_* constant with normal integer results in following
    sparse warnings. Fix it.
    
     fs/open.c:662:21: warning: restricted fmode_t degrades to integer
     fs/anon_inodes.c:123:34: warning: restricted fmode_t degrades to integer
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2828,9 +2828,10 @@ int proc_nr_files(struct ctl_table *table, int write,
 int __init get_filesystem_list(char *buf);
 
 #define __FMODE_EXEC		((__force int) FMODE_EXEC)
+#define __FMODE_NONOTIFY	((__force int) FMODE_NONOTIFY)
 
 #define OPEN_FMODE(flag) ((__force fmode_t)(((flag + 1) & O_ACCMODE) | \
-					    (flag & FMODE_NONOTIFY)))
+					    (flag & __FMODE_NONOTIFY)))
 
 #endif /* __KERNEL__ */
 #endif /* _LINUX_FS_H */
