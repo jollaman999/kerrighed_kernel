84afb9eb3e5d4d870534320466f9640de570fa16
ish <ish@innogrid.com>
2019-10-02 21:36:08 +0900

    10/02 - fs: proc: base
    * Reverted patches
    https://github.com/torvalds/linux/commit/c2c0bb44620dece7ec97e28167e32c343da22867
    https://github.com/torvalds/linux/commit/2ca66ff70a49f0c92352282d6a67d47be090a462
    https://github.com/torvalds/linux/commit/21a6457a79a02908dc8e60fe820828b2cc72d13a

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -82,6 +82,7 @@
 #include <linux/elf.h>
 #include <linux/pid_namespace.h>
 #include <linux/fs_struct.h>
+
 #ifdef CONFIG_KRG_KDDM
 #include <kerrighed/krgnodemask.h>
 #include <kddm/kddm.h>
@@ -92,6 +93,7 @@
 #ifdef CONFIG_KRG_FAF
 #include <kerrighed/faf.h>
 #endif
+
 #include "internal.h"
 
 /* NOTE:
@@ -292,205 +294,45 @@ struct mm_struct *mm_for_maps(struct task_struct *task)
 	return mm;
 }
 
-static ssize_t proc_pid_cmdline_read(struct file *file, char __user *buf,
-				     size_t _count, loff_t *pos)
+#if !defined(CONFIG_KRG_PROCFS) || !defined(CONFIG_KRG_PROC)
+static
+#endif
+int proc_pid_cmdline(struct task_struct *task, char *buffer)
 {
-	struct task_struct *tsk;
-	struct mm_struct *mm;
-	char *page;
-	unsigned long count = _count;
-	unsigned long arg_start, arg_end, env_start, env_end;
-	unsigned long len1, len2, len;
-	unsigned long p;
-	char c;
-	ssize_t rv;
-
-	BUG_ON(*pos < 0);
-
-	tsk = get_proc_task(file->f_dentry->d_inode);
-	if (!tsk)
-		return -ESRCH;
-	mm = get_task_mm(tsk);
-	put_task_struct(tsk);
+	int res = 0;
+	unsigned int len;
+	struct mm_struct *mm = get_task_mm(task);
 	if (!mm)
-		return 0;
-	/* Check if process spawned far enough to have cmdline. */
-	if (!mm->env_end) {
-		rv = 0;
-		goto out_mmput;
-	}
+		goto out;
+	if (!mm->arg_end)
+		goto out_mm;	/* Shh! No looking before we're done */
 
-	page = (char *)__get_free_page(GFP_TEMPORARY);
-	if (!page) {
-		rv = -ENOMEM;
-		goto out_mmput;
-	}
+	len = mm->arg_end - mm->arg_start;
 
-	down_read(&mm->mmap_sem);
-	arg_start = mm->arg_start;
-	arg_end = mm->arg_end;
-	env_start = mm->env_start;
-	env_end = mm->env_end;
-	up_read(&mm->mmap_sem);
+	if (len > PAGE_SIZE)
+		len = PAGE_SIZE;
 
-	BUG_ON(arg_start > arg_end);
-	BUG_ON(env_start > env_end);
+	res = access_process_vm(task, mm->arg_start, buffer, len, 0);
 
-	len1 = arg_end - arg_start;
-	len2 = env_end - env_start;
-
-	/*
-	 * Inherently racy -- command line shares address space
-	 * with code and data.
-	 */
-	rv = access_remote_vm(mm, arg_end - 1, &c, 1, 0);
-	if (rv <= 0)
-		goto out_free_page;
-
-	rv = 0;
-
-	if (c == '\0') {
-		/* Command line (set of strings) occupies whole ARGV. */
-		if (len1 <= *pos)
-			goto out_free_page;
-
-		p = arg_start + *pos;
-		len = len1 - *pos;
-		while (count > 0 && len > 0) {
-			unsigned int _count;
-			int nr_read;
-
-			_count = min3(count, len, PAGE_SIZE);
-			nr_read = access_remote_vm(mm, p, page, _count, 0);
-			if (nr_read < 0)
-				rv = nr_read;
-			if (nr_read <= 0)
-				goto out_free_page;
-
-			if (copy_to_user(buf, page, nr_read)) {
-				rv = -EFAULT;
-				goto out_free_page;
-			}
-
-			p       += nr_read;
-			len     -= nr_read;
-			buf     += nr_read;
-			count   -= nr_read;
-			rv      += nr_read;
-		}
-	} else {
-		/*
-		 * Command line (1 string) occupies ARGV and maybe
-		 * extends into ENVP.
-		 */
-		if (len1 + len2 <= *pos)
-			goto skip_argv_envp;
-		if (len1 <= *pos)
-			goto skip_argv;
-
-		p = arg_start + *pos;
-		len = len1 - *pos;
-		while (count > 0 && len > 0) {
-			unsigned int _count, l;
-			int nr_read;
-			bool final;
-
-			_count = min3(count, len, PAGE_SIZE);
-			nr_read = access_remote_vm(mm, p, page, _count, 0);
-			if (nr_read < 0)
-			        rv = nr_read;
-			if (nr_read <= 0)
-			        goto out_free_page;
-
-			/*
-			 * Command line can be shorter than whole ARGV
-			 * even if last "marker" byte says it is not.
-			 */
-			final = false;
-			l = strnlen(page, nr_read);
-			if (l < nr_read) {
-			        nr_read = l;
-			        final = true;
-			}
-
-			if (copy_to_user(buf, page, nr_read)) {
-			        rv = -EFAULT;
-			        goto out_free_page;
-			}
-
-			p       += nr_read;
-			len     -= nr_read;
-			buf     += nr_read;
-			count   -= nr_read;
-			rv      += nr_read;
-
-			if (final)
-			        goto out_free_page;
-		}
-skip_argv:
-		/*
-		 * Command line (1 string) occupies ARGV and
-		 * extends into ENVP.
-		 */
-		if (len1 <= *pos) {
-			p = env_start + *pos - len1;
-			len = len1 + len2 - *pos;
+	// If the nul at the end of args has been overwritten, then
+	// assume application is using setproctitle(3).
+	if (res > 0 && buffer[res-1] != '\0' && len < PAGE_SIZE) {
+		len = strnlen(buffer, res);
+		if (len < res) {
+		    res = len;
 		} else {
-			p = env_start;
-			len = len2;
+			len = mm->env_end - mm->env_start;
+			if (len > PAGE_SIZE - res)
+				len = PAGE_SIZE - res;
+			res += access_process_vm(task, mm->env_start, buffer+res, len, 0);
+			res = strnlen(buffer, res);
 		}
-		while (count > 0 && len > 0) {
-			unsigned int _count, l;
-			int nr_read;
-			bool final;
-
-			_count = min3(count, len, PAGE_SIZE);
-			nr_read = access_remote_vm(mm, p, page, _count, 0);
-			if (nr_read < 0)
-			        rv = nr_read;
-			if (nr_read <= 0)
-			        goto out_free_page;
-
-			/* Find EOS. */
-			final = false;
-			l = strnlen(page, nr_read);
-			if (l < nr_read) {
-			        nr_read = l;
-			        final = true;
-			}
-
-			if (copy_to_user(buf, page, nr_read)) {
-			        rv = -EFAULT;
-			        goto out_free_page;
-			}
-
-			p       += nr_read;
-			len     -= nr_read;
-			buf     += nr_read;
-			count   -= nr_read;
-			rv      += nr_read;
-
-			if (final)
-			        goto out_free_page;
-		}
-skip_argv_envp:
-		;
 	}
-
-out_free_page:
-	free_page((unsigned long)page);
-out_mmput:
+out_mm:
 	mmput(mm);
-	if (rv > 0)
-		*pos += rv;
-	return rv;
+out:
+	return res;
 }
-
-static const struct file_operations proc_pid_cmdline_ops = {
-	.read   = proc_pid_cmdline_read,
-	.llseek = generic_file_llseek,
-};
-
 
 #if !defined(CONFIG_KRG_PROCFS) || !defined(CONFIG_KRG_PROC)
 static
@@ -692,7 +534,10 @@ static const struct file_operations proc_lstats_operations = {
 
 #endif
 
-static int proc_oom_score(struct task_struct *task, char *buffer)
+#if !defined(CONFIG_KRG_PROCFS) || !defined(CONFIG_KRG_PROC)
+static
+#endif
+int proc_oom_score(struct task_struct *task, char *buffer)
 {
 	unsigned long points = 0;
 
@@ -970,6 +815,9 @@ static int proc_fd_access_allowed(struct inode *inode)
 	return allowed;
 }
 
+#if !defined(CONFIG_KRG_PROCFS) || !defined(CONFIG_KRG_PROC)
+static
+#endif
 int proc_setattr(struct dentry *dentry, struct iattr *attr)
 {
 	int error;
@@ -3312,7 +3160,7 @@ static const struct pid_entry tgid_base_stuff[] = {
 #ifdef CONFIG_HAVE_ARCH_TRACEHOOK
 	INF("syscall",    S_IRUGO, proc_pid_syscall),
 #endif
-	REG("cmdline",    S_IRUGO, proc_pid_cmdline_ops),
+	INF("cmdline",    S_IRUGO, proc_pid_cmdline),
 	ONE("stat",       S_IRUGO, proc_tgid_stat),
 	ONE("statm",      S_IRUGO, proc_pid_statm),
 	REG("maps",       S_IRUGO, proc_maps_operations),
@@ -3710,7 +3558,7 @@ static const struct pid_entry tid_base_stuff[] = {
 #ifdef CONFIG_HAVE_ARCH_TRACEHOOK
 	INF("syscall",   S_IRUGO, proc_pid_syscall),
 #endif
-	REG("cmdline",   S_IRUGO, proc_pid_cmdline_ops),
+	INF("cmdline",   S_IRUGO, proc_pid_cmdline),
 	ONE("stat",      S_IRUGO, proc_tid_stat),
 	ONE("statm",     S_IRUGO, proc_pid_statm),
 #ifdef CONFIG_KRG_KDDM
