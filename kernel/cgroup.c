4b7eba708225d5f98b8eda56c3a02478c898fb5e
EdwardSon <ehdwn1991@gmail.com>
2019-10-05 17:11:42 +0900

    [kernel/cgroup.c] 이상 없음

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -54,6 +54,10 @@
 #include <linux/cgroupstats.h>
 #include <linux/hash.h>
 #include <linux/namei.h>
+#ifdef CONFIG_KRG_EPM
+#include <kerrighed/ghost.h>
+#include <kerrighed/action.h>
+#endif
 #include <linux/smp_lock.h>
 #include <linux/pid_namespace.h>
 #include <linux/idr.h>
@@ -4974,3 +4978,40 @@ struct cgroup_subsys debug_subsys = {
 	.subsys_id = debug_subsys_id,
 };
 #endif /* CONFIG_CGROUP_DEBUG */
+
+#ifdef CONFIG_KRG_EPM
+int export_cgroups(struct epm_action *action,
+		   ghost_t *ghost, struct task_struct *task)
+{
+	int err = 0;
+
+	/* TODO */
+	if (task->cgroups != &init_css_set)
+		err = -EBUSY;
+
+	return err;
+}
+
+int import_cgroups(struct epm_action *action,
+		   ghost_t *ghost, struct task_struct *task)
+{
+	/* TODO */
+	get_css_set(&init_css_set);
+	task->cgroups = &init_css_set;
+	INIT_LIST_HEAD(&task->cg_list);
+
+	return 0;
+}
+
+void unimport_cgroups(struct task_struct *task)
+{
+	/* TODO */
+	cgroup_exit(task, 0);
+}
+
+void free_ghost_cgroups(struct task_struct *ghost)
+{
+	/* TODO */
+	cgroup_exit(ghost, 0);
+}
+#endif /* CONFIG_KRG_EPM */
