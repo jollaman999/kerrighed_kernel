jeanparpaillon/kerrighed-kernel

remove/fs: Kill FAF files served by leaving nodes
FAF accesses in progress to a leaving node are cancelled.

FAF client files for which the server is leaving are tagged as dead so
that any further operation on them fails with -EIO.

FAF server files are closed on leaving nodes, as well as associated FAF poll
metadata.

Signed-off-by: Louis Rilling <louis.rilling@kerlabs.com>

kerrighed/fs/faf/faf.c
@@ -68,12 +68,7 @@ out:
 	return res;
 }
 
-/** Close a file in the FAF deamon.
- *  @author Renaud Lottiaux
- *
- *  @param file    The file to close.
- */
-int close_faf_file(struct file * file)
+static int __close_faf_file(struct file *file)
 {
         struct files_struct *files = first_krgrpc->files;
         struct file * faf_file;
@@ -81,7 +76,6 @@ int close_faf_file(struct file * file)
         int fd = file->f_faf_srv_index;
 
 	BUG_ON (!(file->f_flags & O_FAF_SRV));
-	BUG_ON (file_count(file) != 1);
 
 	/* Remove the file from the FAF server file table */
 
@@ -107,6 +101,17 @@ int close_faf_file(struct file * file)
 	file->f_flags = file->f_flags & (~O_FAF_SRV);
 
         return filp_close(faf_file, files);
+}
+
+/** Close a file in the FAF deamon.
+ *  @author Renaud Lottiaux
+ *
+ *  @param file    The file to close.
+ */
+int close_faf_file(struct file *file)
+{
+	BUG_ON(file_count(file) != 1);
+	return __close_faf_file(file);
 }
 
 /** Check if we need to close a FAF server file.
@@ -166,12 +171,69 @@ void check_close_faf_srv_file(struct file *file)
 	__check_close_faf_srv_file(objid, file);
 }
 
+static void close_faf_srv_file(unsigned long objid, struct file *file)
+{
+	struct dvfs_file_struct *dvfs_file;
+	bool close_file = false;
+
+	dvfs_file = get_dvfs_file_struct(objid);
+	/* If dvfs file is NULL, someone else did the job before us */
+	if (dvfs_file->file) {
+		BUG_ON(dvfs_file->file != file);
+		dvfs_file->file = NULL;
+		close_file = true;
+	}
+	put_dvfs_file_struct (objid);
+
+	if (close_file)
+		__close_faf_file(file);
+}
+
+/* Called by faf_remove_local() */
+void check_close_faf_srv_files(void)
+{
+	struct files_struct *files = first_krgrpc->files;
+	struct file *file;
+	unsigned long dvfs_id = 0; /* quiet gcc */
+	int fd, max_fd;
+
+	max_fd = files_fdtable(files)->max_fds;
+	for (fd = 0; fd < max_fd; fd++) {
+		spin_lock(&files->file_lock);
+		file = fcheck_files(files, fd);
+		if (file) {
+			if (file->f_flags & O_FAF_SRV)
+				dvfs_id = file->f_objid;
+			else
+				file = NULL;
+		}
+		spin_unlock(&files->file_lock);
+		if (file && dvfs_id)
+			close_faf_srv_file(dvfs_id, file);
+	}
+}
+
 void free_faf_file_private_data(struct file *file)
 {
+	struct faf_client_data *data;
+
 	if (!(file->f_flags & O_FAF_CLT))
 		return;
 
-	kmem_cache_free (faf_client_data_cachep, file->private_data);
+	data = file->private_data;
+	/* Optimization check */
+	if (!data->server_dead) {
+		down_write(&client_list_sem[data->server_id]);
+		/* Double check */
+		if (!data->server_dead)
+			list_del(&data->list);
+		up_write(&client_list_sem[data->server_id]);
+	}
+	/* Matches smp_wmb() in hotplug.c:handle_faf_invalidate() */
+	smp_read_barrier_depends();
+	/* If we observed data->server_dead != 0, then data->list is unlinked */
+
+	kmem_cache_free (faf_client_data_cachep, data);
 	file->private_data = NULL;
 }
 
@@ -184,7 +246,7 @@ void free_faf_file_private_data(struct file *file)
 void check_last_faf_client_close(struct file *file,
 				 struct dvfs_file_struct *dvfs_file)
 {
-	faf_client_data_t *data;
+	faf_client_data_t *data = file->private_data;
 	struct faf_notify_msg msg;
 
 	if(!(file->f_flags & O_FAF_CLT))
@@ -194,13 +256,14 @@ void check_last_faf_client_close(struct file *file,
 	 * being for the FAF server node. In this case, notify the FAF server
 	 * to let it check if it should close the FAF file or not.
 	 */
-	if (dvfs_file->count == 1) {
-		data = file->private_data;
+	if ((dvfs_file->count == 1) && faf_srv_hold(data)) {
 		msg.server_fd = data->server_fd;
 		msg.objid = file->f_objid;
 
 		rpc_async(RPC_FAF_NOTIFY_CLOSE, data->server_id,
 			  &msg, sizeof(msg));
+
+		faf_srv_release(data);
 	}
 
 	free_faf_file_private_data(file);
@@ -284,6 +347,7 @@ void faf_init ()
 	ruaccess_start();
 	faf_server_init();
 	faf_hooks_init();
+	faf_hotplug_init();
 
 	printk("FAF: initialisation : done\n");
 }

kerrighed/fs/faf/faf_file_mgr.c
@@ -58,6 +58,19 @@ struct file *create_faf_file_from_krg_desc (struct task_struct *task,
 	file->f_pos = desc->f_pos;
 	file->private_data = data;
 
+	/*
+	 * FAF server being removed may race invalidating other clients.
+	 * However, if it is online we are guaranteed that it will remain online
+	 * until the toplevel operation (migration, etc.) completes.
+	 */
+	if (!krgnode_online(data->server_id)) {
+		data->server_dead = 1;
+	} else if (!data->server_dead) {
+		down_write(&client_list_sem[data->server_id]);
+		list_add(&data->list, &client_list[data->server_id]);
+		up_write(&client_list_sem[data->server_id]);
+	}
+
 exit:
 	return file;
 }
@@ -73,6 +86,7 @@ void fill_faf_file_krg_desc(faf_client_data_t *data, struct file *file)
 	data->f_flags = flags;
 	data->f_mode = file->f_mode;
 	data->f_pos = file->f_pos;
+	data->server_dead = 0;
 	data->server_id = kerrighed_node_id;
 	data->server_fd = file->f_faf_srv_index;
 	data->i_mode = file->f_dentry->d_inode->i_mode;

kerrighed/fs/faf/faf_internal.h
@@ -7,11 +7,16 @@
 #ifndef __FAF__
 #define __FAF__
 
+#include <linux/list.h>
+#include <linux/rwsem.h>
 #include <linux/wait.h>
 #include <kerrighed/faf.h>
+#include <kerrighed/sys/types.h>
+#include <kerrighed/krgnodemask.h>
 
 struct epm_action;
 struct dvfs_file_struct;
+struct remote_sleepers_queue;
 
 /*--------------------------------------------------------------------------*
  *                                                                          *
@@ -23,12 +28,26 @@ struct dvfs_file_struct;
 
 /*--------------------------------------------------------------------------*
  *                                                                          *
+ *                              EXTERN VARIABLES                            *
+ *                                                                          *
+ *--------------------------------------------------------------------------*/
+
+extern struct remote_sleepers_queue faf_remote_sleepers;
+extern struct list_head client_list[KERRIGHED_MAX_NODES];
+extern struct rw_semaphore client_list_sem[KERRIGHED_MAX_NODES];
+extern struct rw_semaphore faf_srv_hotplug_rwsem;
+
+/*--------------------------------------------------------------------------*
+ *                                                                          *
  *                              EXTERN FUNCTIONS                            *
  *                                                                          *
  *--------------------------------------------------------------------------*/
 
 void faf_init (void);
 void faf_finalize (void);
+void faf_hotplug_init(void);
+void faf_polled_fd_remove_local(void);
+int faf_remove_local(const krgnodemask_t *nodes);
 
 int check_activate_faf(struct task_struct *tsk, int index, struct file *file,
 		       struct epm_action *action);
@@ -37,6 +56,10 @@ void check_last_faf_client_close(struct file *file,
 				 struct dvfs_file_struct *dvfs_file);
 void __check_close_faf_srv_file(unsigned long objid, struct file *file);
 void check_close_faf_srv_file(struct file *file);
+void check_close_faf_srv_files(void);
 void free_faf_file_private_data(struct file *file);
+
+bool faf_srv_hold(struct faf_client_data *data);
+void faf_srv_release(struct faf_client_data *data);
 
 #endif // __FAF__

kerrighed/fs/faf/hotplug.c
@@ -1,0 +1,99 @@
+/**
+ *  Implementation of faf related hotplug mechanisms.
+ *  @file hotplug.c
+ *
+ *  Copyright (C) 2009, Louis Rilling, Kerlabs.
+ */
+
+#include <linux/remote_sleep.h>
+#include <linux/list.h>
+#include <linux/rwsem.h>
+#include <kerrighed/sys/types.h>
+#include <kerrighed/krginit.h>
+#include <kerrighed/krgnodemask.h>
+#include <net/krgrpc/rpc.h>
+#include <net/krgrpc/rpcid.h>
+
+#include "faf_internal.h"
+
+struct list_head client_list[KERRIGHED_MAX_NODES];
+struct rw_semaphore client_list_sem[KERRIGHED_MAX_NODES];
+DECLARE_RWSEM(faf_srv_hotplug_rwsem);
+
+bool faf_srv_hold(struct faf_client_data *data)
+{
+	bool ret;
+
+	if (data->server_dead)
+		return false;
+
+	down_read(&client_list_sem[data->server_id]);
+	ret = !data->server_dead;
+	if (!ret)
+		up_read(&client_list_sem[data->server_id]);
+
+	return ret;
+}
+
+void faf_srv_release(struct faf_client_data *data)
+{
+	up_read(&client_list_sem[data->server_id]);
+}
+
+static int handle_faf_invalidate(struct rpc_desc *desc,
+				 void *_msg,
+				 size_t size)
+{
+	kerrighed_node_t node = *(kerrighed_node_t *)_msg;
+	struct faf_client_data *data, *safe;
+
+	down_write(&client_list_sem[node]);
+	list_for_each_entry_safe(data, safe, &client_list[node], list) {
+		list_del(&data->list);
+		/*
+		 * Matches smp_read_barrier_depends() in
+		 * faf.c:free_faf_file_private_data()
+		 */
+		smp_wmb();
+		data->server_dead = 1;
+	}
+	up_write(&client_list_sem[node]);
+
+	return 0;
+}
+
+int faf_remove_local(const krgnodemask_t *nodes)
+{
+	krgnodemask_t to_invalidate;
+	int err;
+
+	remote_sleepers_cancel(&faf_remote_sleepers);
+
+	krgnodes_or(to_invalidate, krgnode_online_map, *nodes);
+	krgnode_clear(kerrighed_node_id, to_invalidate);
+	err = rpc_sync_m(RPC_FAF_INVALIDATE, &to_invalidate,
+			 &kerrighed_node_id, sizeof(kerrighed_node_id));
+	if (err)
+		return err;
+
+	down_write(&faf_srv_hotplug_rwsem);
+	check_close_faf_srv_files();
+	faf_polled_fd_remove_local();
+	up_write(&faf_srv_hotplug_rwsem);
+
+	remote_sleepers_enable(&faf_remote_sleepers);
+
+	return err;
+}
+
+void faf_hotplug_init(void)
+{
+	kerrighed_node_t n;
+
+	for (n = 0; n < KERRIGHED_MAX_NODES; n++) {
+		INIT_LIST_HEAD(&client_list[n]);
+		init_rwsem(&client_list_sem[n]);
+	}
+
+	rpc_register_int(RPC_FAF_INVALIDATE, handle_faf_invalidate, 0);
+}

jollaman999/kerrighed_kernel

kerrighed/fs/faf/faf.c
@@ -81,12 +81,7 @@ out:
 	return res;
 }
 
-/** Close a file in the FAF deamon.
- *  @author Renaud Lottiaux
- *
- *  @param file    The file to close.
- */
-int close_faf_file(struct file * file)
+static int __close_faf_file(struct file *file)
 {
         struct files_struct *files = first_krgrpc->files;
         struct file * faf_file;
@@ -94,7 +89,6 @@ int close_faf_file(struct file * file)
         int fd = file->f_faf_srv_index;
 
 	BUG_ON (!(file->f_flags & O_FAF_SRV));
-	BUG_ON (file_count(file) != 1);
 
 	/* Remove the file from the FAF server file table */
 
@@ -125,6 +119,17 @@ int close_faf_file(struct file * file)
 	file->f_objid = 0;
 
         return filp_close(faf_file, files);
+}
+
+/** Close a file in the FAF deamon.
+ *  @author Renaud Lottiaux
+ *
+ *  @param file    The file to close.
+ */
+int close_faf_file(struct file *file)
+{
+	BUG_ON(file_count(file) != 1);
+	return __close_faf_file(file);
 }
 
 /** Check if we need to close a FAF server file.
@@ -184,12 +189,69 @@ void check_close_faf_srv_file(struct file *file)
 	__check_close_faf_srv_file(objid, file);
 }
 
+static void close_faf_srv_file(unsigned long objid, struct file *file)
+{
+	struct dvfs_file_struct *dvfs_file;
+	bool close_file = false;
+
+	dvfs_file = get_dvfs_file_struct(objid);
+	/* If dvfs file is NULL, someone else did the job before us */
+	if (dvfs_file->file) {
+		BUG_ON(dvfs_file->file != file);
+		dvfs_file->file = NULL;
+		close_file = true;
+	}
+	put_dvfs_file_struct (objid);
+
+	if (close_file)
+		__close_faf_file(file);
+}
+
+/* Called by faf_remove_local() */
+void check_close_faf_srv_files(void)
+{
+	struct files_struct *files = first_krgrpc->files;
+	struct file *file;
+	unsigned long dvfs_id = 0; /* quiet gcc */
+	int fd, max_fd;
+
+	max_fd = files_fdtable(files)->max_fds;
+	for (fd = 0; fd < max_fd; fd++) {
+		spin_lock(&files->file_lock);
+		file = fcheck_files(files, fd);
+		if (file) {
+			if (file->f_flags & O_FAF_SRV)
+				dvfs_id = file->f_objid;
+			else
+				file = NULL;
+		}
+		spin_unlock(&files->file_lock);
+		if (file && dvfs_id)
+			close_faf_srv_file(dvfs_id, file);
+	}
+}
+
 void free_faf_file_private_data(struct file *file)
 {
+	struct faf_client_data *data;
+
 	if (!(file->f_flags & O_FAF_CLT))
 		return;
 
-	kmem_cache_free (faf_client_data_cachep, file->private_data);
+	data = file->private_data;
+	/* Optimization check */
+	if (!data->server_dead) {
+		spin_lock(&faf_client_list_lock[data->server_id]);
+		/* Double check */
+		if (!data->server_dead)
+			list_del(&data->list);
+		spin_unlock(&faf_client_list_lock[data->server_id]);
+	}
+	/* Matches smp_wmb() in hotplug.c:handle_faf_invalidate() */
+	smp_read_barrier_depends();
+	/* If we observed data->server_dead != 0, then data->list is unlinked */
+
+	kmem_cache_free (faf_client_data_cachep, data);
 	file->private_data = NULL;
 }
 
@@ -202,7 +264,7 @@ void free_faf_file_private_data(struct file *file)
 void check_last_faf_client_close(struct file *file,
 				 struct dvfs_file_struct *dvfs_file)
 {
-	faf_client_data_t *data;
+	faf_client_data_t *data = file->private_data;
 	struct faf_notify_msg msg;
 
 	if(!(file->f_flags & O_FAF_CLT))
@@ -212,13 +274,14 @@ void check_last_faf_client_close(struct file *file,
 	 * being for the FAF server node. In this case, notify the FAF server
 	 * to let it check if it should close the FAF file or not.
 	 */
-	if (dvfs_file->count == 1) {
-		data = file->private_data;
+	if ((dvfs_file->count == 1) && faf_srv_hold(data)) {
 		msg.server_fd = data->server_fd;
 		msg.objid = file->f_objid;
 
 		rpc_async(RPC_FAF_NOTIFY_CLOSE, data->server_id,
 			  &msg, sizeof(msg));
+
+		faf_srv_release(data);
 	}
 
 	free_faf_file_private_data(file);
@@ -302,6 +365,7 @@ void faf_init ()
 	ruaccess_start();
 	faf_server_init();
 	faf_hooks_init();
+	faf_hotplug_init();
 
 	printk("FAF: initialisation : done\n");
 }

kerrighed/fs/faf/faf_file_mgr.c
@@ -58,6 +58,19 @@ struct file *create_faf_file_from_krg_desc (struct task_struct *task,
 	file->f_pos = desc->f_pos;
 	file->private_data = data;
 
+	/*
+	 * FAF server being removed may race invalidating other clients.
+	 * However, if it is online we are guaranteed that it will remain online
+	 * until the toplevel operation (migration, etc.) completes.
+	 */
+	if (!krgnode_online(data->server_id)) {
+		data->server_dead = 1;
+	} else if (!data->server_dead) {
+		spin_lock(&faf_client_list_lock[data->server_id]);
+		list_add(&data->list, &faf_client_list[data->server_id]);
+		spin_unlock(&faf_client_list_lock[data->server_id]);
+	}
+
 exit:
 	return file;
 }
@@ -73,6 +86,7 @@ void fill_faf_file_krg_desc(faf_client_data_t *data, struct file *file)
 	data->f_flags = flags;
 	data->f_mode = file->f_mode;
 	data->f_pos = file->f_pos;
+	data->server_dead = 0;
 	data->server_id = kerrighed_node_id;
 	data->server_fd = file->f_faf_srv_index;
 	data->i_mode = file->f_dentry->d_inode->i_mode;

kerrighed/fs/faf/faf_internal.h
@@ -7,11 +7,17 @@
 #ifndef __FAF__
 #define __FAF__
 
+#include <linux/list.h>
+#include <linux/spinlock.h>
+#include <linux/rwsem.h>
 #include <linux/wait.h>
 #include <kerrighed/faf.h>
+#include <kerrighed/sys/types.h>
+#include <kerrighed/krgnodemask.h>
 
 struct epm_action;
 struct dvfs_file_struct;
+struct remote_sleepers_queue;
 
 /*--------------------------------------------------------------------------*
  *                                                                          *
@@ -23,12 +29,27 @@ struct dvfs_file_struct;
 
 /*--------------------------------------------------------------------------*
  *                                                                          *
+ *                              EXTERN VARIABLES                            *
+ *                                                                          *
+ *--------------------------------------------------------------------------*/
+
+extern struct remote_sleepers_queue faf_remote_sleepers;
+extern struct list_head faf_client_list[KERRIGHED_MAX_NODES];
+extern spinlock_t faf_client_list_lock[KERRIGHED_MAX_NODES];
+extern struct rw_semaphore faf_client_sem[KERRIGHED_MAX_NODES];
+extern struct rw_semaphore faf_srv_hotplug_rwsem;
+
+/*--------------------------------------------------------------------------*
+ *                                                                          *
  *                              EXTERN FUNCTIONS                            *
  *                                                                          *
  *--------------------------------------------------------------------------*/
 
 void faf_init (void);
 void faf_finalize (void);
+void faf_hotplug_init(void);
+void faf_polled_fd_remove_local(void);
+int faf_remove_local(const krgnodemask_t *nodes);
 
 int check_activate_faf(struct task_struct *tsk, int index, struct file *file,
 		       struct epm_action *action);
@@ -37,6 +58,10 @@ void check_last_faf_client_close(struct file *file,
 				 struct dvfs_file_struct *dvfs_file);
 void __check_close_faf_srv_file(unsigned long objid, struct file *file);
 void check_close_faf_srv_file(struct file *file);
+void check_close_faf_srv_files(void);
 void free_faf_file_private_data(struct file *file);
+
+bool faf_srv_hold(struct faf_client_data *data);
+void faf_srv_release(struct faf_client_data *data);
 
 #endif // __FAF__

kerrighed/fs/faf/hotplug.c
@@ -1,0 +1,104 @@
+/**
+ *  Implementation of faf related hotplug mechanisms.
+ *  @file hotplug.c
+ *
+ *  Copyright (C) 2009, Louis Rilling, Kerlabs.
+ */
+
+#include <linux/remote_sleep.h>
+#include <linux/list.h>
+#include <linux/spinlock.h>
+#include <linux/rwsem.h>
+#include <kerrighed/sys/types.h>
+#include <kerrighed/krginit.h>
+#include <kerrighed/krgnodemask.h>
+#include <net/krgrpc/rpc.h>
+#include <net/krgrpc/rpcid.h>
+
+#include "faf_internal.h"
+
+struct list_head faf_client_list[KERRIGHED_MAX_NODES];
+spinlock_t faf_client_list_lock[KERRIGHED_MAX_NODES];
+struct rw_semaphore faf_client_sem[KERRIGHED_MAX_NODES];
+DECLARE_RWSEM(faf_srv_hotplug_rwsem);
+
+bool faf_srv_hold(struct faf_client_data *data)
+{
+	bool ret;
+
+	if (data->server_dead)
+		return false;
+
+	down_read(&faf_client_sem[data->server_id]);
+	ret = !data->server_dead;
+	if (!ret)
+		up_read(&faf_client_sem[data->server_id]);
+
+	return ret;
+}
+
+void faf_srv_release(struct faf_client_data *data)
+{
+	up_read(&faf_client_sem[data->server_id]);
+}
+
+static int handle_faf_invalidate(struct rpc_desc *desc,
+				 void *_msg,
+				 size_t size)
+{
+	kerrighed_node_t node = *(kerrighed_node_t *)_msg;
+	struct faf_client_data *data, *safe;
+
+	down_write(&faf_client_sem[node]);
+	spin_lock(&faf_client_list_lock[node]);
+	list_for_each_entry_safe(data, safe, &faf_client_list[node], list) {
+		list_del(&data->list);
+		/*
+		 * Matches smp_read_barrier_depends() in
+		 * faf.c:free_faf_file_private_data()
+		 */
+		smp_wmb();
+		data->server_dead = 1;
+	}
+	spin_unlock(&faf_client_list_lock[node]);
+	up_write(&faf_client_sem[node]);
+
+	return 0;
+}
+
+int faf_remove_local(const krgnodemask_t *nodes)
+{
+	krgnodemask_t to_invalidate;
+	int err;
+
+	remote_sleepers_cancel(&faf_remote_sleepers);
+
+	krgnodes_or(to_invalidate, krgnode_online_map, *nodes);
+	krgnode_clear(kerrighed_node_id, to_invalidate);
+	err = rpc_sync_m(RPC_FAF_INVALIDATE, &to_invalidate,
+			 &kerrighed_node_id, sizeof(kerrighed_node_id));
+	if (err)
+		return err;
+
+	down_write(&faf_srv_hotplug_rwsem);
+	check_close_faf_srv_files();
+	faf_polled_fd_remove_local();
+	up_write(&faf_srv_hotplug_rwsem);
+
+	remote_sleepers_enable(&faf_remote_sleepers);
+
+	return err;
+}
+
+void faf_hotplug_init(void)
+{
+	kerrighed_node_t n;
+
+	for (n = 0; n < KERRIGHED_MAX_NODES; n++) {
+		INIT_LIST_HEAD(&faf_client_list[n]);
+		spin_lock_init(&faf_client_list_lock[n]);
+		init_rwsem(&faf_client_sem[n]);
+	}
+
+	rpc_register_int(RPC_FAF_INVALIDATE, handle_faf_invalidate, 0);
+}