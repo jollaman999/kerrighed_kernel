jeanparpaillon/kerrighed-kernel

krg_ns/pid: Record krg_ns instead of krg_ns->root_pid_ns in pid_ns
This makes available a task's krg_ns after exit_notify(), which will be
required by zombie migration.

Signed-off-by: Louis Rilling <louis.rilling@kerlabs.com>

kernel/pid_namespace.c
@@ -115,8 +115,8 @@ static void destroy_pid_namespace(struct pid_namespace *ns)
 	int i;
 
 #ifdef CONFIG_KRG_PROC
-	if (ns->krg_ns_root && ns->krg_ns_root != ns)
-		put_pid_ns(ns->krg_ns_root);
+	if (ns->krg_ns && krg_pid_ns_root(ns) != ns)
+		put_krg_ns(ns->krg_ns);
 #endif
 	for (i = 0; i < PIDMAP_ENTRIES; i++)
 		kfree(ns->pidmap[i].page);
@@ -142,9 +142,9 @@ struct pid_namespace *copy_pid_ns(unsigned long flags, struct pid_namespace *old
 	{
 		new_ns->global = old_ns->global;
 		new_ns->global |= current->create_krg_ns;
-		if (old_ns->krg_ns_root)
-			get_pid_ns(old_ns->krg_ns_root);
-		new_ns->krg_ns_root = old_ns->krg_ns_root;
+		if (old_ns->krg_ns)
+			get_krg_ns(old_ns->krg_ns);
+		new_ns->krg_ns = old_ns->krg_ns;
 		new_ns->parent = get_pid_ns(old_ns);
 	}
 #else

jollaman999/kerrighed_kernel

kernel/pid_namespace.c
@@ -121,9 +121,9 @@ static struct pid_namespace *create_pid_namespace(struct pid_namespace *parent_p
 	if (accept_parent) {
 		ns->global = parent_pid_ns->global;
 		ns->global |= current->create_krg_ns;
-		if (parent_pid_ns->krg_ns_root)
-			get_pid_ns(parent_pid_ns->krg_ns_root);
-		ns->krg_ns_root = parent_pid_ns->krg_ns_root;
+		if (parent_pid_ns->krg_ns)
+			get_krg_ns(parent_pid_ns->krg_ns);
+		ns->krg_ns = parent_pid_ns->krg_ns;
 		ns->parent = get_pid_ns(parent_pid_ns);
 	}
 #else
@@ -153,8 +153,8 @@ static void destroy_pid_namespace(struct pid_namespace *ns)
 	int i;
 
 #ifdef CONFIG_KRG_PROC
-	if (ns->krg_ns_root && ns->krg_ns_root != ns)
-		put_pid_ns(ns->krg_ns_root);
+	if (ns->krg_ns && krg_pid_ns_root(ns) != ns)
+		put_krg_ns(ns->krg_ns);
 #endif
 	proc_free_inum(ns->proc_inum);
 	for (i = 0; i < PIDMAP_ENTRIES; i++)