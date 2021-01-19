e5ea40b55b7932b9a59fc2acea6c089e4b49fe96
ish <ish@innogrid.com>
2021-01-19 14:24:17 +0900

    fs: LOOKUP_EMPTY and AT_EMPTY_PATH, fhandle support
    Referenced from linux-source-2.6.32-openvz-042stab145.3-amd64

--- kernel/sys_ni.c
+++ kernel/sys_ni.c
@@ -185,5 +185,10 @@ cond_syscall(compat_sys_timerfd_gettime);
 cond_syscall(sys_eventfd);
 cond_syscall(sys_eventfd2);
 
+/* open by handle */
+cond_syscall(sys_name_to_handle_at);
+cond_syscall(sys_open_by_handle_at);
+cond_syscall(compat_sys_open_by_handle_at);
+
 /* performance counters: */
 cond_syscall(sys_perf_event_open);
