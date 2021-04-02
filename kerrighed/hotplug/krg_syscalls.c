jeanparpaillon/kerrighed-kernel

Import of the RPC part of Kerrighed

kerrighed/hotplug/krg_syscalls.c
@@ -1,0 +1,175 @@
+/** Interface to create / remove Kerrighed syscalls.
+ *  @file krg_syscalls.c
+ *
+ *  Copyright (C) 2001-2006, INRIA, Universite de Rennes 1, EDF.
+ *  Copyright (C) 2009, Kerlabs
+ */
+#include <linux/proc_fs.h>
+#include <linux/fs.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/hashtable.h>
+
+#define MODULE_NAME "Syscalls interface"
+#define DEBUG_THIS_MODULE
+
+#include <kerrighed/debug.h>
+
+#include <kerrighed/krg_syscalls.h>
+#include <kerrighed/procfs.h>
+
+#define PROC_HASH_TABLE_SIZE 256
+
+struct proc_dir_entry *proc_services = NULL;
+
+hashtable_t *proc_service_functions;
+
+static int proc_services_ioctl(struct inode *inode, struct file *filp,
+			       unsigned int cmd, unsigned long arg);
+static ssize_t proc_services_read(struct file *, char *, size_t, loff_t *);
+
+static struct file_operations proc_services_files_ops = {
+	.ioctl = proc_services_ioctl,
+	.read = proc_services_read,
+};
+
+/** IO Control for the file /proc/kerrighed/services.
+ *  @author Renaud Lottiaux
+ */
+static int proc_services_ioctl(struct inode *inode, struct file *filp,
+			       unsigned int cmd, unsigned long arg)
+{
+	struct proc_service_entry *service_entry;
+
+	service_entry =
+	    (struct proc_service_entry *)hashtable_find(proc_service_functions,
+							cmd);
+
+	if (service_entry != NULL) {
+		service_entry->count++;
+		return service_entry->fct((void *)arg);
+	}
+#ifdef CONFIG_KRG_DEBUG
+	PANIC("Kerrighed command %d-%d (0x%08x) unknown\n", cmd & 0xE0,
+	      cmd & 0x1F, cmd);
+#endif
+
+	return -EINVAL;
+}
+
+static ssize_t proc_services_read(struct file *f, char *buff,
+				  size_t count, loff_t * off)
+{
+	int index;
+	ssize_t ret;
+
+	printk("proc_service_read: start - count=%ld\n", (long)count);
+
+	ret = 0;
+
+	for (index = 0;
+	     (index < proc_service_functions->hashtable_size) && (count >= 50);
+	     index++) {
+		struct hash_list *ht;
+		struct proc_service_entry *se;
+
+		se = proc_service_functions->table[index].data;
+		if (se != NULL) {
+			printk("%s: %lu\n", se->label, se->count);
+
+			for (ht = proc_service_functions->table[index].next;
+			     (ht != NULL) && (count >= 50); ht = ht->next) {
+				int l;
+				se = ht->data;
+				printk("%s: %lu\n", se->label, se->count);
+				l = sprintf(buff + ret, "%s: %lu\n", se->label,
+					    se->count);
+				count -= l;
+				ret += l;
+			};
+		};
+	};
+
+	printk("proc_service_read: stop - (count=%ld)\n", (long)ret);
+	return ret;
+};
+
+/** Add a service to the /proc/kerrighed/services
+ *  @author Renaud Lottiaux
+ *
+ *  @param cmd   Identifier of the service.
+ *  @param fun   Service function.
+ */
+int register_proc_service(unsigned int cmd, proc_service_function_t fun)
+{
+	struct proc_service_entry *service_entry;
+
+	service_entry =
+	    (struct proc_service_entry *)hashtable_find(proc_service_functions,
+							cmd);
+	if (service_entry != NULL && fun != NULL) {
+		PANIC("Kerrighed command %d-%d (0x%08x) already registered\n",
+		      cmd & 0xE0, cmd & 0x1F, cmd);
+		return -1;
+	}
+
+	service_entry = kmalloc(sizeof(struct proc_service_entry), GFP_KERNEL);
+
+	service_entry->fct = fun;
+	sprintf(service_entry->label, "%d-%d (0x%08x)",
+		cmd & 0xE0, cmd & 0x1F, cmd);
+	service_entry->count = 0;
+
+	hashtable_add(proc_service_functions, cmd, service_entry);
+	return 0;
+}
+EXPORT_SYMBOL(register_proc_service);
+
+/** Remove a service from the /proc/kerrighed/services
+ *  @author Renaud Lottiaux
+ *
+ *  @param cmd   Identifier of the service.
+ */
+int unregister_proc_service(unsigned int cmd)
+{
+	if (hashtable_remove(proc_service_functions, cmd) == NULL)
+		return -EINVAL;
+
+	return 0;
+}
+EXPORT_SYMBOL(unregister_proc_service);
+
+/** Initialisation of the /proc/kerrighed/services file.
+ *  @author Renaud Lottiaux
+ */
+int krg_syscalls_init(void)
+{
+	int err = 0;
+
+	printk("Init kerrighed syscall mechanism\n");
+
+	/* Create the /proc/kerrighed/services */
+
+	proc_services = create_proc_entry("services", 0644, proc_kerrighed);
+	if (proc_services == NULL)
+		err = -EMFILE;
+	else {
+		proc_service_functions = hashtable_new(PROC_HASH_TABLE_SIZE);
+		proc_services->proc_fops = &proc_services_files_ops;
+	}
+
+	return err;
+}
+
+/** Destroy of the /proc/kerrighed/services file.
+ *  @author Renaud Lottiaux
+ */
+int krg_syscalls_finalize(void)
+{
+	remove_proc_entry("services", proc_kerrighed);
+
+	hashtable_free(proc_service_functions);
+
+	return 0;
+}

kerrighed/hotplug/krgsyms.c
@@ -1,0 +1,109 @@
+/*
+ *  Copyright (C) 2006-2007, Pascal Gallard, Kerlabs.
+ */
+
+#include <kerrighed/krgsyms.h>
+#include <linux/hashtable.h>
+
+/*****************************************************************************/
+/*                                                                           */
+/*                          KERRIGHED KSYM MANAGEMENT                        */
+/*                                                                           */
+/*****************************************************************************/
+
+
+#define KRGSYMS_HTABLE_SIZE 256
+
+static hashtable_t *krgsyms_htable = NULL;
+static void* krgsyms_table[KRGSYMS_TABLE_SIZE];
+
+int krgsyms_register(enum krgsyms_val v, void* p)
+{
+	if( (v < 0) || (v >= KRGSYMS_TABLE_SIZE) ){
+		printk("krgsyms_register: Incorrect krgsym value (%d)\n", v);
+		BUG();
+		return -1;
+	};
+
+	if(krgsyms_table[v])
+		printk("krgsyms_register_symbol(%d, %p): value already set in table\n",
+					 v, p);
+
+	if(hashtable_find(krgsyms_htable, (unsigned long)p) != NULL)
+	{
+		printk("krgsyms_register_symbol(%d, %p): value already set in htable\n",
+					 v, p);
+		BUG();
+	}
+
+	hashtable_add(krgsyms_htable, (unsigned long)p, (void*)v);
+	krgsyms_table[v] = p;
+
+	return 0;
+};
+
+int krgsyms_unregister(enum krgsyms_val v)
+{
+	void *p;
+
+	if( (v < 0) || (v >= KRGSYMS_TABLE_SIZE) ){
+		printk("krgsyms_unregister: Incorrect krgsym value (%d)\n", v);
+		BUG();
+		return -1;
+	};
+
+	p = krgsyms_table[v];
+	krgsyms_table[v] = NULL;
+	hashtable_remove(krgsyms_htable, (unsigned long)p);
+
+	return 0;
+};
+
+enum krgsyms_val krgsyms_export(void* p)
+{
+	enum krgsyms_val ret;
+
+	ret = (enum krgsyms_val)hashtable_find(krgsyms_htable, (unsigned long)p);
+
+	if ((p!=NULL) && (ret == KRGSYMS_UNDEF))
+	  {
+			printk ("undefined krgsymbol (0x%p)!", p);
+			BUG();
+		}
+
+	return ret;
+};
+
+void* krgsyms_import(enum krgsyms_val v)
+{
+	if( (v < 0) || (v >= KRGSYMS_TABLE_SIZE) ){
+		printk("krgsyms_import: Incorrect krgsym value (%d)\n", v);
+		BUG();
+		return NULL;
+	};
+
+	if ((v!=0) && (krgsyms_table[v] == NULL))
+	{
+		printk ("undefined krgsymbol (%d)\n", v);
+		BUG();
+	}
+
+	return krgsyms_table[v];
+};
+
+int init_krgsyms(void)
+{
+	int i;
+
+	krgsyms_htable = hashtable_new(KRGSYMS_HTABLE_SIZE);
+	if(krgsyms_table == NULL) return -ENOMEM;
+
+	for(i=0;i<KRGSYMS_TABLE_SIZE;i++){
+		krgsyms_table[i] = NULL;
+	};
+
+	return 0;
+};
+
+void cleanup_krgsyms(void){
+};

jollaman999/kerrighed_kernel

kerrighed/hotplug/krg_syscalls.c
@@ -1,0 +1,175 @@
+/** Interface to create / remove Kerrighed syscalls.
+ *  @file krg_syscalls.c
+ *
+ *  Copyright (C) 2001-2006, INRIA, Universite de Rennes 1, EDF.
+ *  Copyright (C) 2009, Kerlabs
+ */
+#include <linux/proc_fs.h>
+#include <linux/fs.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/krg_hashtable.h>
+
+#define MODULE_NAME "Syscalls interface"
+#define DEBUG_THIS_MODULE
+
+#include <kerrighed/debug.h>
+
+#include <kerrighed/krg_syscalls.h>
+#include <kerrighed/procfs.h>
+
+#define PROC_HASH_TABLE_SIZE 256
+
+struct proc_dir_entry *proc_services = NULL;
+
+hashtable_t *proc_service_functions;
+
+static int proc_services_ioctl(struct inode *inode, struct file *filp,
+			       unsigned int cmd, unsigned long arg);
+static ssize_t proc_services_read(struct file *, char *, size_t, loff_t *);
+
+static struct file_operations proc_services_files_ops = {
+	.ioctl = proc_services_ioctl,
+	.read = proc_services_read,
+};
+
+/** IO Control for the file /proc/kerrighed/services.
+ *  @author Renaud Lottiaux
+ */
+static int proc_services_ioctl(struct inode *inode, struct file *filp,
+			       unsigned int cmd, unsigned long arg)
+{
+	struct proc_service_entry *service_entry;
+
+	service_entry =
+	    (struct proc_service_entry *)hashtable_find(proc_service_functions,
+							cmd);
+
+	if (service_entry != NULL) {
+		service_entry->count++;
+		return service_entry->fct((void *)arg);
+	}
+#ifdef CONFIG_KRG_DEBUG
+	PANIC("Kerrighed command %d-%d (0x%08x) unknown\n", cmd & 0xE0,
+	      cmd & 0x1F, cmd);
+#endif
+
+	return -EINVAL;
+}
+
+static ssize_t proc_services_read(struct file *f, char *buff,
+				  size_t count, loff_t * off)
+{
+	int index;
+	ssize_t ret;
+
+	printk("proc_service_read: start - count=%ld\n", (long)count);
+
+	ret = 0;
+
+	for (index = 0;
+	     (index < proc_service_functions->hashtable_size) && (count >= 50);
+	     index++) {
+		struct hash_list *ht;
+		struct proc_service_entry *se;
+
+		se = proc_service_functions->table[index].data;
+		if (se != NULL) {
+			printk("%s: %lu\n", se->label, se->count);
+
+			for (ht = proc_service_functions->table[index].next;
+			     (ht != NULL) && (count >= 50); ht = ht->next) {
+				int l;
+				se = ht->data;
+				printk("%s: %lu\n", se->label, se->count);
+				l = sprintf(buff + ret, "%s: %lu\n", se->label,
+					    se->count);
+				count -= l;
+				ret += l;
+			};
+		};
+	};
+
+	printk("proc_service_read: stop - (count=%ld)\n", (long)ret);
+	return ret;
+};
+
+/** Add a service to the /proc/kerrighed/services
+ *  @author Renaud Lottiaux
+ *
+ *  @param cmd   Identifier of the service.
+ *  @param fun   Service function.
+ */
+int register_proc_service(unsigned int cmd, proc_service_function_t fun)
+{
+	struct proc_service_entry *service_entry;
+
+	service_entry =
+	    (struct proc_service_entry *)hashtable_find(proc_service_functions,
+							cmd);
+	if (service_entry != NULL && fun != NULL) {
+		PANIC("Kerrighed command %d-%d (0x%08x) already registered\n",
+		      cmd & 0xE0, cmd & 0x1F, cmd);
+		return -1;
+	}
+
+	service_entry = kmalloc(sizeof(struct proc_service_entry), GFP_KERNEL);
+
+	service_entry->fct = fun;
+	sprintf(service_entry->label, "%d-%d (0x%08x)",
+		cmd & 0xE0, cmd & 0x1F, cmd);
+	service_entry->count = 0;
+
+	hashtable_add(proc_service_functions, cmd, service_entry);
+	return 0;
+}
+EXPORT_SYMBOL(register_proc_service);
+
+/** Remove a service from the /proc/kerrighed/services
+ *  @author Renaud Lottiaux
+ *
+ *  @param cmd   Identifier of the service.
+ */
+int unregister_proc_service(unsigned int cmd)
+{
+	if (hashtable_remove(proc_service_functions, cmd) == NULL)
+		return -EINVAL;
+
+	return 0;
+}
+EXPORT_SYMBOL(unregister_proc_service);
+
+/** Initialisation of the /proc/kerrighed/services file.
+ *  @author Renaud Lottiaux
+ */
+int krg_syscalls_init(void)
+{
+	int err = 0;
+
+	printk("Init kerrighed syscall mechanism\n");
+
+	/* Create the /proc/kerrighed/services */
+
+	proc_services = create_proc_entry("services", 0644, proc_kerrighed);
+	if (proc_services == NULL)
+		err = -EMFILE;
+	else {
+		proc_service_functions = hashtable_new(PROC_HASH_TABLE_SIZE);
+		proc_services->proc_fops = &proc_services_files_ops;
+	}
+
+	return err;
+}
+
+/** Destroy of the /proc/kerrighed/services file.
+ *  @author Renaud Lottiaux
+ */
+int krg_syscalls_finalize(void)
+{
+	remove_proc_entry("services", proc_kerrighed);
+
+	hashtable_free(proc_service_functions);
+
+	return 0;
+}

kerrighed/hotplug/krgsyms.c
@@ -1,0 +1,109 @@
+/*
+ *  Copyright (C) 2006-2007, Pascal Gallard, Kerlabs.
+ */
+
+#include <kerrighed/krgsyms.h>
+#include <linux/krg_hashtable.h>
+
+/*****************************************************************************/
+/*                                                                           */
+/*                          KERRIGHED KSYM MANAGEMENT                        */
+/*                                                                           */
+/*****************************************************************************/
+
+
+#define KRGSYMS_HTABLE_SIZE 256
+
+static hashtable_t *krgsyms_htable = NULL;
+static void* krgsyms_table[KRGSYMS_TABLE_SIZE];
+
+int krgsyms_register(enum krgsyms_val v, void* p)
+{
+	if( (v < 0) || (v >= KRGSYMS_TABLE_SIZE) ){
+		printk("krgsyms_register: Incorrect krgsym value (%d)\n", v);
+		BUG();
+		return -1;
+	};
+
+	if(krgsyms_table[v])
+		printk("krgsyms_register_symbol(%d, %p): value already set in table\n",
+					 v, p);
+
+	if(hashtable_find(krgsyms_htable, (unsigned long)p) != NULL)
+	{
+		printk("krgsyms_register_symbol(%d, %p): value already set in htable\n",
+					 v, p);
+		BUG();
+	}
+
+	hashtable_add(krgsyms_htable, (unsigned long)p, (void*)v);
+	krgsyms_table[v] = p;
+
+	return 0;
+};
+
+int krgsyms_unregister(enum krgsyms_val v)
+{
+	void *p;
+
+	if( (v < 0) || (v >= KRGSYMS_TABLE_SIZE) ){
+		printk("krgsyms_unregister: Incorrect krgsym value (%d)\n", v);
+		BUG();
+		return -1;
+	};
+
+	p = krgsyms_table[v];
+	krgsyms_table[v] = NULL;
+	hashtable_remove(krgsyms_htable, (unsigned long)p);
+
+	return 0;
+};
+
+enum krgsyms_val krgsyms_export(void* p)
+{
+	enum krgsyms_val ret;
+
+	ret = (enum krgsyms_val)hashtable_find(krgsyms_htable, (unsigned long)p);
+
+	if ((p!=NULL) && (ret == KRGSYMS_UNDEF))
+	  {
+			printk ("undefined krgsymbol (0x%p)!", p);
+			BUG();
+		}
+
+	return ret;
+};
+
+void* krgsyms_import(enum krgsyms_val v)
+{
+	if( (v < 0) || (v >= KRGSYMS_TABLE_SIZE) ){
+		printk("krgsyms_import: Incorrect krgsym value (%d)\n", v);
+		BUG();
+		return NULL;
+	};
+
+	if ((v!=0) && (krgsyms_table[v] == NULL))
+	{
+		printk ("undefined krgsymbol (%d)\n", v);
+		BUG();
+	}
+
+	return krgsyms_table[v];
+};
+
+int init_krgsyms(void)
+{
+	int i;
+
+	krgsyms_htable = hashtable_new(KRGSYMS_HTABLE_SIZE);
+	if(krgsyms_table == NULL) return -ENOMEM;
+
+	for(i=0;i<KRGSYMS_TABLE_SIZE;i++){
+		krgsyms_table[i] = NULL;
+	};
+
+	return 0;
+};
+
+void cleanup_krgsyms(void){
+};