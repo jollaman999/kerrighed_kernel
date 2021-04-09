84bb88ab32f9e10336ce3b34d9bb1989474073d9
ish <ish@innogrid.com>
2021-04-10 04:52:47 +0900

    ipc: sem: preempt improve in handle_ipcsem_wakeup_process()

--- ipc/sem_handler.c
+++ ipc/sem_handler.c
@@ -315,6 +315,7 @@ void handle_ipcsem_wakeup_process(struct rpc_desc *desc, void *_msg,
 
 	BUG();
 found:
+	preempt_disable();
 	q->status = 1; /* IN_WAKEUP; */
 
 	BUG_ON(!q->sleeper);
@@ -324,6 +325,7 @@ found:
 	wake_up_process(q->sleeper);
 	smp_wmb();
 	q->status = msg->error;
+	preempt_enable();
 
 out_unlock:
 	sem_unlock(sma, -1);
