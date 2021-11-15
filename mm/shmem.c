47d0943f26ab9f1e5ea6ca49c5d58b90eca01414
jollaman999 <admin@jollaman999.com>
2021-11-15 11:34:13 +0900

    Revert "SHM_UNLOCK: fix Unevictable pages stranded after swap"
    This reverts commit eab50fbfa2507c85981e698ff14ba61c6c0a473c.

--- mm/shmem.c
+++ mm/shmem.c
@@ -424,7 +424,7 @@ static int shmem_free_swap(struct address_space *mapping,
 /*
  * Pagevec may contain swap entries, so shuffle up pages before releasing.
  */
-static void shmem_deswap_pagevec(struct pagevec *pvec)
+static void shmem_pagevec_release(struct pagevec *pvec)
 {
 	int i, j;
 
@@ -434,36 +434,7 @@ static void shmem_deswap_pagevec(struct pagevec *pvec)
 			pvec->pages[j++] = page;
 	}
 	pvec->nr = j;
-}
-
-/*
- * SysV IPC SHM_UNLOCK restore Unevictable pages to their evictable lists.
- */
-void shmem_unlock_mapping(struct address_space *mapping)
-{
-	struct pagevec pvec;
-	pgoff_t indices[PAGEVEC_SIZE];
-	pgoff_t index = 0;
-
-	pagevec_init(&pvec, 0);
-	/*
-	 * Minor point, but we might as well stop if someone else SHM_LOCKs it.
-	 */
-	while (!mapping_unevictable(mapping)) {
-		/*
-		 * Avoid pagevec_lookup(): find_get_pages() returns 0 as if it
-		 * has finished, if it hits a row of PAGEVEC_SIZE swap entries.
-		 */
-		pvec.nr = shmem_find_get_pages_and_swap(mapping, index,
-					PAGEVEC_SIZE, pvec.pages, indices);
-		if (!pvec.nr)
-			break;
-		index = indices[pvec.nr - 1] + 1;
-		shmem_deswap_pagevec(&pvec);
-		check_move_unevictable_pages(pvec.pages, pvec.nr);
-		pagevec_release(&pvec);
-		cond_resched();
-	}
+	pagevec_release(pvec);
 }
 
 /*
@@ -522,8 +493,7 @@ static void shmem_undo_range(struct inode *inode, loff_t lstart, loff_t lend,
 			}
 			unlock_page(page);
 		}
-		shmem_deswap_pagevec(&pvec);
-		pagevec_release(&pvec);
+		shmem_pagevec_release(&pvec);
 		mem_cgroup_uncharge_end();
 		cond_resched();
 		index++;
@@ -605,8 +575,7 @@ static void shmem_undo_range(struct inode *inode, loff_t lstart, loff_t lend,
 			}
 			unlock_page(page);
 		}
-		shmem_deswap_pagevec(&pvec);
-		pagevec_release(&pvec);
+		shmem_pagevec_release(&pvec);
 		mem_cgroup_uncharge_end();
 		index++;
 	}
@@ -3045,10 +3014,6 @@ int shmem_unuse(swp_entry_t swap, struct page *page)
 int shmem_lock(struct file *file, int lock, struct user_struct *user)
 {
 	return 0;
-}
-
-void shmem_unlock_mapping(struct address_space *mapping)
-{
 }
 
 void shmem_truncate_range(struct inode *inode, loff_t lstart, loff_t lend)
