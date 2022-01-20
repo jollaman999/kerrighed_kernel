1176798d3bf1c9435da73034e57f44c06157d3a1
jollaman999 <admin@jollaman999.com>
2022-01-20 17:27:06 +0900

    Revert "mm: fix false-positive warning on exit due mm_nr_pmds(mm)"
    This reverts commit a29c19ad0105831f8c5cf287c610039b939ecf34.

--- mm/mmap.c
+++ mm/mmap.c
@@ -2751,6 +2751,11 @@ void exit_mmap(struct mm_struct *mm)
 	 */
 	while (vma)
 		vma = remove_vma(vma);
+
+	WARN_ON(atomic_long_read(&mm->nr_ptes) >
+			round_up(FIRST_USER_ADDRESS, PMD_SIZE) >> PMD_SHIFT);
+	WARN_ON(mm_nr_pmds(mm) >
+			round_up(FIRST_USER_ADDRESS, PUD_SIZE) >> PUD_SHIFT);
 }
 
 /* Insert vm structure into process list sorted by address
