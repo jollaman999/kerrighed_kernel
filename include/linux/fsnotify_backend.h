198257386fe7136cbb1890274f750f44dd9ee46a
jollaman999 <admin@jollaman999.com>
2022-06-03 21:40:41 +0900

    Revert "fsnotify: mount point listeners list and global mask"
    This reverts commit 667895adcc39eaf7e727e92852ca758cebd02bde.

--- include/linux/fsnotify_backend.h
+++ include/linux/fsnotify_backend.h
@@ -99,10 +99,6 @@ struct fsnotify_group {
 	 * or fsnotify_grp_srcu depending on write vs read.
 	 */
 	struct list_head inode_group_list;
-	/*
-	 * same as above except anchored by fsnotify_vfsmount_groups
-	 */
-	struct list_head vfsmount_group_list;
 
 	/*
 	 * Defines all of the event types in which this group is interested.
@@ -141,7 +137,6 @@ struct fsnotify_group {
 
 	/* prevents double list_del of group_list.  protected by global fsnotify_grp_mutex */
 	bool on_inode_group_list;
-	bool on_vfsmount_group_list;
 
 	/* groups can define private fields here or use the void *private */
 	union {
