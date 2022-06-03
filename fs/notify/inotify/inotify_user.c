d166fa7283769d5db92a2ecbbb16dc932d25b561
jollaman999 <admin@jollaman999.com>
2022-06-03 21:40:28 +0900

    Revert "inotify: use container_of instead of casting"
    This reverts commit 6e8248282ffadf51799ca068ba09e64e4420f40b.

--- fs/notify/inotify/inotify_user.c
+++ fs/notify/inotify/inotify_user.c
@@ -453,9 +453,7 @@ skip_send_ignore:
 /* ding dong the mark is dead */
 static void inotify_free_mark(struct fsnotify_mark_entry *entry)
 {
-	struct inotify_inode_mark_entry *ientry;
-
-	ientry = container_of(entry, struct inotify_inode_mark_entry, fsn_entry);
+	struct inotify_inode_mark_entry *ientry = (struct inotify_inode_mark_entry *)entry;
 
 	kmem_cache_free(inotify_inode_mark_cachep, ientry);
 }
