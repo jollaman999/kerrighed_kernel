8855963863aae539b26c52289480ea9825f3cd24
jollaman999 <admin@jollaman999.com>
2022-06-03 22:47:41 +0900

    Revert "Revert "vfs: Implement proper O_SYNC semantics""
    This reverts commit 1fba97336641e17b3eaa717d980ccc8a0a40004b.

--- fs/xfs/linux-2.6/xfs_ioctl.c
+++ fs/xfs/linux-2.6/xfs_ioctl.c
@@ -641,7 +641,7 @@ xfs_ioc_space(
 	if (filp->f_flags & (O_NDELAY|O_NONBLOCK))
 		attr_flags |= XFS_ATTR_NONBLOCK;
 
-	if (filp->f_flags & O_SYNC)
+	if (filp->f_flags & O_DSYNC)
 		attr_flags |= XFS_ATTR_SYNC;
 
 	if (ioflags & IO_INVIS)
